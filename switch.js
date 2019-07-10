d22fe7cda7800547d95e7c96b06520853d6b7378
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-07-10 21:13:01 +0600

    added a file

---
+++ switch.js
@@ -0,0 +1,36 @@
+//switch_statement in javascript//
+
+var date = new date()
+//0=sunday,1=monday,2=tuesday,3=wednesday//
+
+var today = get.Date()
+
+switch(3){
+    case 0 :
+    console.log('this is  sunday')
+    break 
+    
+    case 1 :
+    console.log('this is monday');
+    break
+    
+    case 2 :
+    console.log('this is tuesday');
+    break
+    
+    case 3 :
+    console.log('this is wednesday');
+    break 
+    
+    case 4 :
+    console.log('this is thursday');
+    break
+    
+    case 5 :
+    console.log('this is friday');
+    break
+    
+    case 6:
+    console.log('this is saturday');
+    break    
+}
